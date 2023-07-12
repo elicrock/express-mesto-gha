1f88caab46b2ec07712f3e1bc5d43a0debf5f1e4
elicrock <rockelic@yandex.ru>
2023-07-13 00:39:45 +0300

    Sprint 13 - fix after tests for message

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const cardRouter = require('./cards');
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
 
-router.use((req, res) => res.status(404).send({ error: 'Страница не найдена!' }));
+router.use((req, res) => res.status(404).send({ message: 'Страница не найдена!' }));
 
 module.exports = router;
