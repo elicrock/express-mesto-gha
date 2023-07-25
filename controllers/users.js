1d038b4326505a278e1c727622c51bbff8cf8f5e
elicrock <rockelic@yandex.ru>
2023-07-25 18:12:50 +0300

    Sprint 14 - fix after 1st review

--- controllers/users.js
+++ controllers/users.js
@@ -95,6 +95,7 @@ const login = (req, res, next) => {
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: true });
+      res.send({ message: 'Авторизация прошла успешно!' });
     })
     .catch(next);
 };
