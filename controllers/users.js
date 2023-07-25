45f40ea10a8c234e69b8ca21e9ff98ad6a873949
Paul <paul.ivanov1990@gmail.com>
2023-07-25 13:53:56 +0300

    Sprint 14 - fix after test

--- controllers/users.js
+++ controllers/users.js
@@ -100,7 +100,7 @@ const login = (req, res, next) => {
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, { httpOnly: true, maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: true });
-      res.send(token);
+      res.send({ token });
     })
     .catch(next);
 };
