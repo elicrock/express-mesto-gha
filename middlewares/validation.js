cfb8f20ccfc6254ea2c85a51f1a4fbad4f033515
Paul <paul.ivanov1990@gmail.com>
2023-07-26 16:28:37 +0300

    Sprint 14 - upd regex url

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,6 @@
 const { celebrate, Joi } = require('celebrate');
 
-const regex = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]+#?$)/;
+const regex = /^https?:\/\/(www\.)?[a-zA-Z0-9-._~:/?#@!$&'()*+,;=%]*/;
 
 const validateGetUserById = celebrate({
   params: Joi.object().keys({
